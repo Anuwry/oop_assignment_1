b4a2950724a7d76177cb25304095f1e81dd0cfca
Anuwry <narakorn0647833428@gmail.com>
2025-11-12 22:14:25 +0700

    Second Commit

---
+++ oop_solution/library_oop.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, book_id, title, author, available_copies):
+        self.id = book_id
+        self.title = title
+        self.author = author
+        self.available_copies = available_copies
+        self.total_copies = available_copies
